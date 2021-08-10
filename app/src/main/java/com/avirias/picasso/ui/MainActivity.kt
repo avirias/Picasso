ec4dad2a50ab9e592f703aeddd9290dc63f88226
Avinash Kumar <avirias@live.com>
2021-08-10 18:57:04 +0530

    Setup home navigation graph with fragments

--- app/src/main/java/com/avirias/picasso/ui/MainActivity.kt
+++ app/src/main/java/com/avirias/picasso/ui/MainActivity.kt
@@ -2,11 +2,23 @@ package com.avirias.picasso.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
 import com.avirias.picasso.R
+import com.avirias.picasso.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(binding.navHostFragment.id) as NavHostFragment
+
+        val navController = navHostFragment.navController
     }
 }
